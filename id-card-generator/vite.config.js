4db244c8ed2c743e6036705c313ee0d42e664541
pushpanjal <pushpanjalshukla@gmail.com>
2026-05-14 19:57:07 +0530

    fixed vercel

--- id-card-generator/vite.config.js
+++ id-card-generator/vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',              // ← YEH ADD KARO
-  server: {
-    port: 3000,
-    open: true
+  base: '/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
   }
 })
